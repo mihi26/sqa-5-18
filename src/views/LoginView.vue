30ebb1ba61f0c513f15553fcb9805105a2595e57
Minh Hieu <minhhieuu26@gmail.com>
2023-03-06 17:35:58 +0700

    init project

---
+++ src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+</script>
+
+<template>
+  <div class="login-wrapper">
+    <div class="info-wrapper">
+      <input v-model="username" class="input-wrapper" />
+      <input v-model="password" class="input-wrapper" />
+    </div>
+    <div class="button-wrapper">
+      <button>LOGIN</button>
+      <div class="forgot-password">Forgot password?</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 43px;
+
+  .info-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .input-wrapper {
+    width: 100%;
+    min-height: 45px;
+  }
+}
+</style>
